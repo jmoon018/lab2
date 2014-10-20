03656ca2de3e8b61a853e97b375f826db41ffd76
Jamal Moon <jmoon018@ucr.edu>
2014-10-20 11:50:54 -0700

    Added fork.cpp

---
+++ fork.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	// FORK FOR LETTER 'A'
+	int success = fork();
+
+	if(success == -1)
+	{
+		perror("Fork failed...");
+	}
+	else if(success == 0) // child process
+	{
+		for(int i = 0; i < 10000; i++)
+		{
+			cout << "A";
+		}
+		cout << endl;
+	}
+	else // parent process.. must print PID
+	{
+		cout << "PID: " << success << endl; // print pid
+	}
+	return 0;
+}
